7ae20d91ce5eef18ed00abb0fb6f533ebffe7475
Seth Pyle <seth.pyle86@gmail.com>
2021-11-04 14:01:58 -0500

    fixes for pg backup job

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
 helm upgrade --install homelab homelab/ \
 --set postgres.user=$PG_USER \
 --set postgres.pass=$PG_PASS \
---set postgres.db=homelab
+--set postgres.db=homelab \
+--set aws.key=$HOMELAB_AWS_ACCESS_KEY_ID \
+--set aws.secret=$HOMELAB_AWS_ACCESS_SECRET_KEY_ID \
+--set pgBackup.s3Bucket=$PGBACKUP_S3_BUCKET
